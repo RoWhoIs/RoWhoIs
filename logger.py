6e75431122a87e26cf3914d07c295013d2beeb16
Autumn <autumn@autmn.net>
2024-01-27 13:12:39 -0700

    Merge from aut-mn/AsyncLogger

--- logger.py
+++ logger.py
@@ -1,6 +1,5 @@
-import asyncio
+import asyncio, aiofiles
 from datetime import datetime
-import aiofiles
 
 class AsyncLogCollector:
     def __init__(self, filename):
@@ -11,8 +10,7 @@ class AsyncLogCollector:
         self.log_levels = {'INFO': '\033[32mINFO\033[0m','WARN': '\033[33mWARN\033[0m','ERROR': '\033[31mERROR\033[0m','FATAL': '\033[31;1mFATAL\033[0m'}
     async def log(self, level, message):
         timestamp = self.get_colored_timestamp()
-        formatted_message = self.log_format % {'timestamp': timestamp, 'level': self.log_levels.get(level, level), 'message': message}
-        print(formatted_message)
+        print(formatted_message = self.log_format % {'timestamp': timestamp, 'level': self.log_levels.get(level, level), 'message': message})
         plain_log = self.log_format % {'timestamp': self.get_timestamp(), 'level': level, 'message': message}
         async with aiofiles.open(self.filename, mode='a') as file: await file.write(plain_log + '\n')
     async def info(self, message): await self.log('INFO', message)
@@ -22,6 +20,4 @@ class AsyncLogCollector:
     def get_colored_timestamp(self): return '\033[90m' + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + '\033[0m'
     def get_timestamp(self): return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     async def start_log_collection(self):
-        while True:
-            log_entry = await self.log_queue.get()
-            print(log_entry)
+        while True: print(await self.log_queue.get())
