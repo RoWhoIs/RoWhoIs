5fcb181d5b02589aeb3782df3931ffe42fb043d3
Autumn <autumn@autmn.net>
2024-02-12 12:06:22 -0700

    Fix #1, prep to implement #2

--- Roquest.py
+++ Roquest.py
@@ -1,107 +1,92 @@
-import random, json
 import aiohttp, asyncio
 from secret import RWI
 from logger import AsyncLogCollector
 
 log_collector = AsyncLogCollector("logs/Roquest.log")
-x_csrf_token = None
-proxy_urls, available_proxies = [], []
+lastProxy = None
 
-async def load_config():
-    global log_token_failures, proxy_urls, proxy_pw, proxy_us, enable_proxying, proxy_auth_required
-    message_shown = False
-    while True:
-        try:
-            with open('config.json', 'r') as file: config = json.load(file)
-            log_token_failures = config.get("Roquest", {}).get("log_token_failures", False)
-            enable_proxying = config.get("Proxy", {}).get("enable_proxying", False)
-            if enable_proxying:
-                await log_collector.info("In config.json: enable_proxying set to True. Roquests will be proxied.")
-                proxy_urls.extend([id for module_data in config.values() if 'proxy_urls' in module_data for id in module_data['proxy_urls']])
-                proxy_pw = config.get("Proxy", {}).get("password", False)
-                proxy_us = config.get("Proxy", {}).get("username", False)
-                if proxy_us and proxy_pw == "": proxy_auth_required = False
-                else: proxy_auth_required = True
-            elif not message_shown: await log_collector.info("In config.json: enable_proxying set to False. Roquests will not be proxied.")
-            if not log_token_failures and not message_shown: await log_collector.info("In config.json: log_token_failures set to False. X-CSRF-TOKEN calls will not be logged.")
-            message_shown = True
-            await asyncio.sleep(3600)
-        except Exception as e:
-            await log_collector.error(f"Failed to update Roquest configuration data: {e}")
-            await asyncio.sleep(10)
+def set_configs(enable_proxying:bool, proxy_urls, username:str, password):
+    global enableProxying, proxyUrls, proxyCredentials, proxyPool
+    enableProxying, proxyUrls, proxyPool = enable_proxying, proxy_urls, []
+    if username != "": proxyCredentials = aiohttp.BasicAuth(login=username, password=password)
+    else: proxyCredentials = None
+    if enableProxying: loop.create_task(proxy_handler())
 
-async def TestProxies():
-    global available_proxies
-    no_proxies_message_shown = False
-    while enable_proxying:
-        if proxy_auth_required: proxy_auth = aiohttp.BasicAuth(proxy_us, proxy_pw)
-        else: proxy_auth = None
+async def proxy_handler():
+    global enableProxying, proxyUrls, proxyCredentials, proxyPool
+    proxiesMessageShown = False
+    while enableProxying:
         async def test_proxy(session, proxy_url):
             try:
-                async with session.get("https://auth.roblox.com/", proxy=proxy_url, proxy_auth=proxy_auth, timeout=10) as response:
+                async with session.get("https://auth.roblox.com/", proxy=proxy_url, proxy_auth=proxyCredentials, timeout=2) as response:
                     if response.status == 200: return True
-            except Exception as e: pass
+            except Exception: pass
             return False
         async with aiohttp.ClientSession() as session:
-            tasks = [test_proxy(session, proxy_url) for proxy_url in proxy_urls]
-            results = await asyncio.gather(*tasks)
-            available_proxies = [proxy_url for proxy_url, result in zip(proxy_urls, results) if result]
-            if len(available_proxies) <= 0 and not no_proxies_message_shown:
-                await log_collector.warn("No usable proxies found! Fallbacking to non-proxied.")
-                no_proxies_message_shown = True
-        await asyncio.sleep(600)
+            if len(proxyUrls) <= 0 and not proxiesMessageShown: await log_collector.warn("No usable proxies found! Fallbacking to non-proxied.")
+            else:
+                tasks = [test_proxy(session, proxy_url) for proxy_url in proxyUrls]
+                results = await asyncio.gather(*tasks)
+                proxyPool = [proxy_url for proxy_url, result in zip(proxyUrls, results) if result]
+                if len(proxyPool) <= 0 and not proxiesMessageShown:
+                    await log_collector.warn("No usable proxies found! Fallbacking to non-proxied.")
+                    proxiesMessageShown = True
+                elif not proxiesMessageShown:
+                    proxiesMessageShown = True
+                    await log_collector.info(f"Proxy pool has {len(proxyPool)} usable IPs and RoWhoIs will proxy roquests.")
+        await asyncio.sleep(300)
 
-async def TokenRenew():
-    global RoliData
-    while True:
-        try:
-            if log_token_failures: await log_collector.info('Renewing X-CSRF-TOKEN')
-            async with aiohttp.ClientSession(cookies={".ROBLOSECURITY": RWI.RSEC}) as session:
-                async with session.get('https://users.roblox.com/v1/users/authenticated') as resp:
-                    if resp.status == 200:
-                        renew_token = resp.headers.get('x-csrf-token')
-                        if renew_token is None and log_token_failures: await log_collector.error("Failed to get X-CSRF token. X-CSRF-TOKEN not found in headers")
-                        else:
-                            x_csrf_token = renew_token
-                            if log_token_failures: await log_collector.info('Successfully renewed X-CSRF-TOKEN')
-                    elif log_token_failures: await log_collector.error(f"Failed to get X-CSRF-TOKEN. Status code: {resp.status}")
-                    await asyncio.sleep(180)
-        except Exception as e:
-            if log_token_failures: await log_collector.error(f"An unexpected error occurred during X-CSRF-TOKEN renewal: {e}")
-            await asyncio.sleep(10)
+async def proxy_picker(currentProxy, didError:bool):
+    try:
+        global proxyPool
+        if not enableProxying:  return None
+        if didError and currentProxy != None:
+            for proxy in proxyPool:
+                if proxy == currentProxy: proxyPool.remove(proxy)
+        if len(proxyPool) == 0:  return None
+        if currentProxy is None: return proxyPool[0]
+        else:
+            try:
+                index = proxyPool.index(currentProxy)
+                next_index = (index + 1) % len(proxyPool)
+                return proxyPool[next_index]
+            except ValueError:
+                if len(proxyPool) != 0 and didError:
+                    for proxy in proxyPool:
+                        if proxy == currentProxy: proxyPool.remove(proxy)
+                if len(proxyPool) == 0: return None
+                return proxyPool[0]
+    except Exception as e:
+        await log_collector.error(f"Encountered severe error while picking proxy: {e}")
+        return None
 
 loop = asyncio.get_event_loop()
-loop.create_task(load_config())
-loop.create_task(TestProxies())
-loop.create_task(TokenRenew())
 
-async def Roquest(method, url, max_retries=3, retry_interval=2, **kwargs):
+async def Roquest(method, url, max_retries=3, retry_interval=3, **kwargs):
+    global proxyCredentials, lastProxy
     method = method.lower()
-    headers = {"x-csrf-token": f"{x_csrf_token}"}
-    if enable_proxying and len(available_proxies) >= 1: 
-        proxy = f"http://{available_proxies[random.randint(0, len(available_proxies))]}"
-        await log_collector.info(f"Setting proxy to: {proxy}")
-        if proxy_auth_required: proxy_auth = aiohttp.BasicAuth(proxy_us, proxy_pw)
-        else: proxy_auth = None
-    else: proxy, proxy_auth = None, None
     async with aiohttp.ClientSession(cookies={".ROBLOSECURITY": RWI.RSEC}) as main_session:
-        await log_collector.info(f'Performing Roquest with method {method} to {url}')
+        proxy = await proxy_picker(lastProxy, False)
+        lastProxy = proxy
+        await log_collector.info(f"Performing Roquest with method {method} to {url} {('on proxy ' + proxy) if proxy != None else ''}")
         for retry in range(max_retries):
             try:
-                async with main_session.request(method, url, headers=headers, proxy=proxy, proxy_auth=proxy_auth, **kwargs) as resp:
+                async with main_session.request(method, url, proxy=proxy, proxy_auth=proxyCredentials, timeout=3, **kwargs) as resp:
                     if resp.status == 200: return await resp.json()
                     elif resp.status in [403, 404, 400]:
-                        await log_collector.warn(f"Failed to perform Roquest with method {method} to {url}: Got status code {resp.status}")
+                        await log_collector.warn(f"Failed to perform Roquest with method {method} to {url} {('on proxy ' + proxy) if proxy != None else ''}: Got status code {resp.status}. {retry}/{max_retries}")
                         return resp.status
+                    elif resp.status == 429:
+                        proxy = await proxy_picker(lastProxy, False)
+                        lastProxy = proxy
                     else:
-                        await log_collector.warn(f"Roquest to {url} with method {method} failed with status code {resp.status}. Retrying...")
+                        await log_collector.warn(f"Roquest to {url} with method {method} failed with status code {resp.status} {('on proxy ' + proxy) if proxy != None else ''}. Retrying...")
                         await asyncio.sleep(retry_interval)
-            except Exception as e:
-                await log_collector.error(f"An error occurred during the request: {e}")
-                if enable_proxying and len(available_proxies) >= 1: # Switch proxies in case it was a bad proxy
-                    proxy = f"http://{available_proxies[random.randint(0, len(available_proxies))]}"
-                    await log_collector.info(f"Switching proxies to: {proxy}")
-        await log_collector.error(f"Failed to make a successful Roquest after {max_retries} retries to {url} ({method})")
+            except Exception as e:  
+                proxy = await proxy_picker(lastProxy, True)
+                lastProxy = proxy
+                await log_collector.error(f"An error occurred during the request {('on proxy ' + proxy) if proxy != None else ''}: {e}")
+        await log_collector.error(f"Failed to make a successful Roquest after {max_retries} retries to {url} ({method}) {('on proxy ' + proxy) if proxy != None else ''}")
         return -1
 
 async def RoliData(max_retries=5, retry_interval=5):
@@ -115,7 +100,7 @@ async def RoliData(max_retries=5, retry_interval=5):
                 else: await log_collector.warn(f"RoliData update failed with status code {resp.status}. Retrying...")
         raise await log_collector.error(f"Failed to make a successful request to RoliData after {max_retries} retries.")
 
-async def GetFileContent(asset_id):
+async def GetFileContent(asset_id): # This will cause issues in the future but proxying larger content seems unstable
     try:
         await log_collector.info(f'Performing GetFileContent for item: {asset_id}')
         async with aiohttp.ClientSession(cookies={".ROBLOSECURITY": RWI.RSEC}) as main_session:
---
+++ main.py
@@ -0,0 +1,45 @@
+import RoWhoIs, Roquest, json, asyncio, subprocess
+from logger import AsyncLogCollector
+
+logCollector = AsyncLogCollector("logs/Server.log")
+
+def sync_logging(errorLevel, errorContent):
+    log_functions = {"fatal": logCollector.fatal,"error": logCollector.error,"warn": logCollector.warn,"info": logCollector.info}
+    asyncio.get_event_loop().run_until_complete(log_functions[errorLevel](errorContent))
+
+def get_version():
+    try:
+        short_commit_id = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).strip()
+        return short_commit_id.decode('utf-8')
+    except subprocess.CalledProcessError as e:
+        sync_logging("error", f"Error getting short commit ID: {e}")
+        return 0
+
+def load_runtime(shortHash):
+    optOut, userBlocklist, staffIds, proxyUrls = [], [], [], []
+    try:
+        # RoWhoIs
+        with open('config.json', 'r') as file: config = json.load(file)
+        testingMode = config.get("RoWhoIs", {}).get("testing", False)
+        if testingMode: sync_logging("warn", "Currently running in testing mode.")
+        else: sync_logging("warn", "Currently running in production mode.")
+        verboseLogging = config.get("RoWhoIs", {}).get("log_config_updates", False)
+        if not verboseLogging: sync_logging("info", "In config.json: log_config_updates set to False. Successful configuration updates will not be logged.")
+        optOut.extend([id for module_data in config.values() if 'opt_out' in module_data for id in module_data['opt_out']])
+        if verboseLogging: sync_logging("info", "Opt-out IDs updated successfully.")
+        userBlocklist.extend([id for module_data in config.values() if 'banned_users' in module_data for id in module_data['banned_users']])
+        if verboseLogging: sync_logging("info", "User blocklist updated successfully.")
+        staffIds.extend([id for module_data in config.values() if 'admin_ids' in module_data for id in module_data['admin_ids']])
+        # Roquest
+        proxyingEnabled = config.get("Proxy", {}).get("proxying_enabled", False)
+        username = config.get("Proxy", {}).get("username", False)
+        password = config.get("Proxy", {}).get("password", False)
+        if password == "": password = None
+        proxyUrls.extend([id for module_data in config.values() if 'proxy_urls' in module_data for id in module_data['proxy_urls']])
+        Roquest.set_configs(proxyingEnabled, proxyUrls, username, password)
+        RoWhoIs.main(testingMode, staffIds, optOut, userBlocklist, verboseLogging, shortHash)
+    except Exception as e: sync_logging("fatal", f"Failed to initialize! Invalid config? {e}")
+
+shortHash = get_version()
+sync_logging("info", f"Initializing RoWhoIs on version {shortHash}...")
+load_runtime(shortHash)
