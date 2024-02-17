17a87361a723890f625e04da2e3f4f8ca1948ba8
Autumn <autumn@autmn.net>
2024-02-16 21:18:23 -0700

    Better API node handling, fallback thumbnails, fix registry updating

--- RoModules.py
+++ RoModules.py
@@ -4,7 +4,7 @@ log_collector = AsyncLogCollector("logs/RoModules.log")
 
 async def convert_to_id(username): # Returns user id, username, display name, verified badge
     try:
-        userid = await Roquest.Roquest("POST", "https://users.roblox.com/v1/usernames/users", json={"usernames": [username],"excludeBannedUsers": False})
+        userid = await Roquest.Roquest("POST", "users", "v1/usernames/users", json={"usernames": [username],"excludeBannedUsers": False})
         if userid[0] in [-1, 403]: return -2, -2, -2, -2
         elif userid[0] in [404, 400]: return -1, -1, -1, -1
         if "data" in userid[1] and userid[1]["data"]:
@@ -18,7 +18,7 @@ async def convert_to_id(username): # Returns user id, username, display name, ve
         
 async def convert_to_username(userid:int):
     try:
-        userid = await Roquest.Roquest("POST", "https://users.roblox.com/v1/users", json={"userIds": [userid],"excludeBannedUsers": False})
+        userid = await Roquest.Roquest("POST", "users", "v1/users", json={"userIds": [userid],"excludeBannedUsers": False})
         if userid[0] in [-1, 403]: return -2, -2
         elif userid[0] in [404, 400]: return -1, -1
         if "data" in userid[1] and userid[1]["data"]:
@@ -32,10 +32,7 @@ async def convert_to_username(userid:int):
 
 async def check_verification(user_id:int):
     try:
-        verifhat, verifsign = await asyncio.gather(
-            Roquest.Roquest("GET", f"https://inventory.roblox.com/v1/users/{user_id}/items/4/102611803"),
-            Roquest.Roquest("GET", f"https://inventory.roblox.com/v1/users/{user_id}/items/4/1567446")
-        )
+        verifhat, verifsign = await asyncio.gather(Roquest.Roquest("GET", "inventory", f"v1/users/{user_id}/items/4/102611803"),Roquest.Roquest("GET", "inventory", f"v1/users/{user_id}/items/4/1567446"))
         hat_owned = verifhat[0] not in [-1, 403, 400, 404] and any("type" in item for item in verifhat[1].get("data", []))
         sign_owned = verifsign[0] not in [-1, 403, 400, 404] and any("type" in item for item in verifsign[1].get("data", []))
         return 4 if hat_owned and sign_owned else 1 if hat_owned else 2 if sign_owned else 3
@@ -45,7 +42,7 @@ async def check_verification(user_id:int):
 
 async def last_online(user_id:int):
     try:
-        last_data = await Roquest.Roquest("POST", "https://presence.roblox.com/v1/presence/last-online", json={"userIds": [user_id]})
+        last_data = await Roquest.Roquest("POST", "presence", "v1/presence/last-online", json={"userIds": [user_id]})
         if last_data[0] in [-1, 403]: return -2
         elif last_data[0] in [404, 400]: return -1
         else: return last_data[1]["lastOnlineTimestamps"][0]["lastOnline"]
@@ -55,29 +52,27 @@ async def last_online(user_id:int):
 
 async def get_player_thumbnail(user_id:int, size):
     try:
-        thumbnail_url = await Roquest.Roquest("GET", f"https://thumbnails.roblox.com/v1/users/avatar?userIds={user_id}&size={size}&format=Png&isCircular=false")
-        if thumbnail_url[0] in [-1, 403]: return -2
-        elif thumbnail_url[0] in [404, 400]: return -1
+        thumbnail_url = await Roquest.Roquest("GET", "thumbnails", f"v1/users/avatar?userIds={user_id}&size={size}&format=Png&isCircular=false")
+        if thumbnail_url[0] in [-1, 403, 404, 400]: return "https://www.robloxians.com/resources/not-available.png"
         elif thumbnail_url[1]["data"][0]["state"] == "Blocked": return "https://robloxians.com/resources/blocked.png"
         else: return thumbnail_url[1]["data"][0]["imageUrl"]
     except Exception as e:
         await log_collector.error(f"Encountered an error while running the get_player_thumbnail function: {e}")
-        return -2
+        return "https://www.robloxians.com/resources/not-available.png"
 
 async def get_item_thumbnail(item_id:int, size):
     try:
-        thumbnail_url = await Roquest.Roquest("GET", f"https://thumbnails.roblox.com/v1/assets?assetIds={item_id}&returnPolicy=PlaceHolder&size={size}&format=Png&isCircular=false")
-        if thumbnail_url[0] in [-1, 403]: return -2
-        elif thumbnail_url[0] in [404, 400]: return -1
+        thumbnail_url = await Roquest.Roquest("GET", "thumbnails", f"v1/assets?assetIds={item_id}&returnPolicy=PlaceHolder&size={size}&format=Png&isCircular=false")
+        if thumbnail_url[0] in [-1, 403, 404, 400]: return "https://www.robloxians.com/resources/not-available.png"
         elif thumbnail_url[1]["data"][0]["state"] == "Blocked": return "https://robloxians.com/resources/blocked.png"
         else: return thumbnail_url[1]["data"][0]["imageUrl"]
     except Exception as e:
         await log_collector.error(f"Encountered an error while running the get_item_thumbnail function: {e}")
-        return -2
+        return "https://www.robloxians.com/resources/not-available.png"
 
 async def get_player_profile(user_id:int): # Returns description, joined, banned, username, display name, verified
     try:
-        desc = await Roquest.Roquest("GET", f"https://users.roblox.com/v1/users/{user_id}")
+        desc = await Roquest.Roquest("GET", "users", f"v1/users/{user_id}")
         if desc[0] in [-1, 403]: return -2, -2, -2, -2, -2, -2
         elif desc[0] in [400, 404]: return -1, -1, -1, -1, -1, -1
         else: return desc[1]["description"], desc[1]["created"], desc[1]["isBanned"], desc[1]["name"], desc[1]["displayName"], desc[1]["hasVerifiedBadge"]
@@ -90,10 +85,10 @@ async def get_previous_usernames(user_id:int):
         usernames = []
         next_page_cursor = None
         while True:
-            url = f"https://users.roblox.com/v1/users/{user_id}/username-history?limit=100&sortOrder=Asc"
+            url = f"v1/users/{user_id}/username-history?limit=100&sortOrder=Asc"
             if next_page_cursor:
                 url += f"&cursor={next_page_cursor}"
-            data = await Roquest.Roquest("GET", url)
+            data = await Roquest.Roquest("GET", "users", url)
             if data[0] in [-1, 403]: return -2
             elif data[0] in [400, 404]: return -1
             usernames += [entry["name"] for entry in data[1]["data"]]
@@ -106,7 +101,7 @@ async def get_previous_usernames(user_id:int):
 
 async def get_group_count(user_id:int):
     try:
-        gdata = await Roquest.Roquest("GET", f"https://groups.roblox.com/v2/users/{user_id}/groups/roles?includeLocked=true")
+        gdata = await Roquest.Roquest("GET", "groups", f"v2/users/{user_id}/groups/roles?includeLocked=true")
         if gdata[0] in [-1, 403]: return -2
         elif gdata[0] in [400, 404]: return -1
         else: return len(gdata[1]['data'])
@@ -117,11 +112,11 @@ async def get_group_count(user_id:int):
 async def get_socials(user_id:int): # Returns Friends, Followers, Following
     try:
         friend_count, following_count, follow_count = await asyncio.gather(
-            Roquest.Roquest("GET", f"https://friends.roblox.com/v1/users/{user_id}/friends/count"),
-            Roquest.Roquest("GET", f"https://friends.roblox.com/v1/users/{user_id}/followings/count"),
-            Roquest.Roquest("GET", f"https://friends.roblox.com/v1/users/{user_id}/followers/count")
+            Roquest.Roquest("GET", "friends", f"v1/users/{user_id}/friends/count"),
+            Roquest.Roquest("GET", "friends", f"v1/users/{user_id}/followings/count"),
+            Roquest.Roquest("GET", "friends", f"v1/users/{user_id}/followers/count")
         )
-        if friend_count[0] in [-1, 403]: return -2, -2, -2
+        if friend_count[0] in [-1, 403]: return -2, -2, -2 # Please optimize at some point, this seems bloated
         elif friend_count[0] in [400, 404]: return -1, -1, -1
         if following_count[0] in [-1, 403]: return -2, -2, -2
         elif following_count[0] in [400, 404]: return -1, -1, -1
@@ -130,11 +125,11 @@ async def get_socials(user_id:int): # Returns Friends, Followers, Following
         return friend_count[1]["count"], follow_count[1]["count"], following_count[1]["count"]
     except Exception as e:
         await log_collector.error(f"Encountered an error while running the get_socials function: {e}")
-        return -2
+        return -2, -2, -2
     
 async def get_friends(user_id:int):
     try:
-        friend_data = await Roquest.Roquest("GET", f"https://friends.roblox.com/v1/users/{user_id}/friends?userSort=0")
+        friend_data = await Roquest.Roquest("GET", "friends", f"v1/users/{user_id}/friends?userSort=0")
         if friend_data[0] in [-1, 403]: return -2
         elif friend_data[0] in [404, 400]: return -1
         else: return friend_data[1]
@@ -144,7 +139,7 @@ async def get_friends(user_id:int):
 
 async def get_groups(user_id:int):
     try:
-        group_data = await Roquest.Roquest("GET", f"https://groups.roblox.com/v1/users/{user_id}/groups/roles")
+        group_data = await Roquest.Roquest("GET", "groups", f"v1/users/{user_id}/groups/roles")
         if group_data[0] in [-1, 403]: return -2
         elif group_data[0] in [404, 400]: return -1
         else: return group_data[1]
@@ -154,48 +149,44 @@ async def get_groups(user_id:int):
     
 async def get_player_headshot(user_id:int, size):
     try:
-        thumbnail_url = await Roquest.Roquest("GET", f"https://thumbnails.roblox.com/v1/users/avatar-headshot?userIds={user_id}&size={size}&format=Png&isCircular=false")
-        if thumbnail_url[0] in [-1, 403]: return -2
-        elif thumbnail_url[0] in [404, 400]: return -1
+        thumbnail_url = await Roquest.Roquest("GET", "thumbnails", f"v1/users/avatar-headshot?userIds={user_id}&size={size}&format=Png&isCircular=false")
+        if thumbnail_url[0] in [-1, 403, 404, 400]: return "https://www.robloxians.com/resources/not-available.png"
         elif thumbnail_url[1]["data"][0]["state"] == "Blocked": return "https://robloxians.com/resources/blocked.png"
         else: return thumbnail_url[1]["data"][0]["imageUrl"]
     except Exception as e:
         await log_collector.error(f"Encountered an error while running the get_player_headshot function: {e}")
-        return -2
+        return "https://www.robloxians.com/resources/not-available.png"
     
 async def get_badge_thumbnail(badge_id:int):
     try:
-        thumbnail_url = await Roquest.Roquest("GET", f"https://thumbnails.roblox.com/v1/badges/icons?badgeIds={badge_id}&size=150x150&format=Png&isCircular=false")
-        if thumbnail_url[0] in [-1, 403]: return -2
-        elif thumbnail_url[0] in [404, 400]: return -1
+        thumbnail_url = await Roquest.Roquest("GET", "thumbnails", f"v1/badges/icons?badgeIds={badge_id}&size=150x150&format=Png&isCircular=false")
+        if thumbnail_url[0] in [-1, 403, 404, 400]: return "https://www.robloxians.com/resources/not-available.png"
         elif thumbnail_url[1]["data"][0]["state"] == "Blocked": return "https://robloxians.com/resources/blocked.png"
         else: return thumbnail_url[1]["data"][0]["imageUrl"]
     except Exception as e:
         await log_collector.error(f"Encountered an error while running the get_badge_thumbnail function: {e}")
-        return -2
+        return "https://www.robloxians.com/resources/not-available.png"
     
 async def get_group_emblem(group:int, size):
     try:
-        thumbnail_url = await Roquest.Roquest("GET", f"https://thumbnails.roblox.com/v1/groups/icons?groupIds={group}&size={size}&format=Png&isCircular=false")
-        if thumbnail_url[0] in [-1, 403]: return -2
-        elif thumbnail_url[0] in [404, 400]: return -1
+        thumbnail_url = await Roquest.Roquest("GET", "thumbnails", f"v1/groups/icons?groupIds={group}&size={size}&format=Png&isCircular=false")
+        if thumbnail_url[0] in [-1, 403, 404, 400]: return "https://www.robloxians.com/resources/not-available.png"
         elif thumbnail_url[1]["data"][0]["state"] == "Blocked": return "https://robloxians.com/resources/blocked.png"
         else: return thumbnail_url[1]["data"][0]["imageUrl"]
     except Exception as e:
         await log_collector.error(f"Encountered an error while running the get_group_emblem function: {e}")
-        return -2
+        return "https://www.robloxians.com/resources/not-available.png"
 
 async def get_rolidata_from_item(rolidata, item): # Returns limited id, name, acronym, rap, and value
     for limited_id, item_data in rolidata["items"].items():
-        if item.lower() in [item_data[0].lower(), item_data[1].lower(), limited_id]:
-            return limited_id, item_data[0], item_data[1], item_data[2], item_data[4]
+        if item.lower() in [item_data[0].lower(), item_data[1].lower(), limited_id]: return limited_id, item_data[0], item_data[1], item_data[2], item_data[4]
     else: return -1, -1, -1, -1, -1
 
 async def get_membership(user:int): # Returns successType, hasPremium, ownedBc, ownedTbc, and ownedObc
-    checkBc = await Roquest.Roquest("GET", f"https://inventory.roblox.com/v1/users/{user}/items/4/24814192")
+    checkBc = await Roquest.Roquest("GET", "inventory", f"v1/users/{user}/items/4/24814192")
     if checkBc[0] != 200:
         if checkBc[1]['errors'][0]['message'] == "The specified user does not exist!": return False, False, False, False, False
-    checkPremium, checkTbc, checkObc = await asyncio.gather(Roquest.Roquest("GET", f"https://premiumfeatures.roblox.com/v1/users/{user}/validate-membership"),Roquest.Roquest("GET", f"https://inventory.roblox.com/v1/users/{user}/items/4/11895536"),Roquest.Roquest("GET", f"https://inventory.roblox.com/v1/users/{user}/items/4/17407931"))
+    checkPremium, checkTbc, checkObc = await asyncio.gather(Roquest.Roquest("GET", "premiumfeatures", f"v1/users/{user}/validate-membership"),Roquest.Roquest("GET", "inventory", f"v1/users/{user}/items/4/11895536"),Roquest.Roquest("GET", "inventory", f"v1/users/{user}/items/4/17407931"))
     hasPremium = checkPremium[0] not in [-1, 403, 400, 404] and checkPremium[1] == True
     ownedBc = checkBc[0] not in [-1, 403, 400, 404] and any("type" in item for item in checkBc[1].get("data", []))
     ownedTbc = checkTbc[0] not in [-1, 403, 400, 404] and any("type" in item for item in checkTbc[1].get("data", []))
@@ -204,7 +195,7 @@ async def get_membership(user:int): # Returns successType, hasPremium, ownedBc, 
 
 async def get_group(group:int): # Returns name (0), description (1), created (2), verified (3), owner (4), shout (5), members (6), public (7), isLocked (8)
     try:
-        getGroup, getGroupV1 = await asyncio.gather(Roquest.Roquest("GET", f"https://groups.roblox.com/v2/groups?groupIds={group}"),Roquest.Roquest("GET", f"https://groups.roblox.com/v1/groups/{group}"))
+        getGroup, getGroupV1 = await asyncio.gather(Roquest.Roquest("GET", "groups", f"v2/groups?groupIds={group}"),Roquest.Roquest("GET", "groups", f"v1/groups/{group}"))
         if getGroup[0] == 200 and getGroupV1[0] == 200:
             groupName = getGroup[1]['data'][0]['name']
             groupDescription = getGroup[1]['data'][0]['description']
